d3821daad586e7bef7b9281dfc925741fe4c9f62
KwonHeesoo <kwonheesoo46@gmail.com>
2023-11-30 08:54:15 +0900

    food commit2

---
+++ src/main/resources/static/js/food.js
@@ -0,0 +1,8 @@
+
+    const modal = document.querySelector('.modal');
+    const btnOpenModal = document.querySelector('.btn-open-modal')
+
+    btnOpenModal.addEventListener("click", ()=>{
+    modal.style.display="flex";
+
+})
