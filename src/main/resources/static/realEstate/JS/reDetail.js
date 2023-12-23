a14cc2a030b0bfc4228a87e7850c5c8e94caf023
chulhee <shinchulhee0614@gmail.com>
2023-12-23 17:47:00 +0900

    board4랑 merge 하기전 commit and push

--- src/main/resources/static/realEstate/JS/reDetail.js
+++ src/main/resources/static/realEstate/JS/reDetail.js
@@ -64,22 +64,23 @@ $(document).ready(function () {
     })
 
 
+    // 여기부터 지도에 관한 내용.
     // 렌더링 되어있는 부동산 주소값을 변수에 할당.
     var address = $('#address').text().trim();
     console.log("여기에 부동산 주소가 찍힐까? " + address) // 주소 출력되는 것 확인함.
 
 
-// 로드뷰 지도 표시 - 신철희 12/22 저녁
+    // 로드뷰 지도 표시 - 신철희 12/22 저녁
     var centerRealEstate = address;
 
     var roadviewContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
     var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
     var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
 
-// 좌표로 지도의 위치 담기
+    // 좌표로 지도의 위치 담기
     var position = "";
 
-// 주소-좌표 변환 객체를 생성합니다
+    // 주소-좌표 변환 객체를 생성합니다
     var geocoder = new kakao.maps.services.Geocoder();
 
     function showLoadView() {
@@ -89,7 +90,7 @@ $(document).ready(function () {
         });
     }
 
-// 주소로 좌표를 검색합니다
+    // 주소로 좌표를 검색합니다
     geocoder.addressSearch(centerRealEstate, function (result, status) {
 
         // 정상적으로 검색이 완료됐으면
